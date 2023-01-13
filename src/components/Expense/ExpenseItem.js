f1382b20ab63de65ab6bbc56c6ae1372d585ba3a
Trung_Hieu_Dev <dangtrunghieusg@gmail.com>
2023-01-13 13:22:07 +0700

    refactor code

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,16 +1,26 @@
-import Card from './Car';
+import { useState } from 'react';
+import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate'
 
 function ExpenseItem(props) {
+
+    // var title = props.title;
+
+    //hook
+    var [title, setTitle] = useState(props.title);
+
+
     const clickHandler = () => {
-        console.log('click');
+        // title = title + ' has been changed';
+        setTitle(title + ' has been changed');
+        console.log(title);
     }
     
     return (
         <Card className="card">
             <ExpenseDate date={props.date} />
             <div>
-                <h2 className="title">{props.title}</h2>
+                <h2 className="title">{title}</h2>
                 <div>${props.amount}</div>
                 <button onClick={clickHandler}>Change Title</button>
             </div>
